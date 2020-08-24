74071742f3e2e4e30b779cc3615aaeb82f8f40ac
Chris <ChrisNosowsky@gmail.com>
2020-08-24 17:13:27 -0400

    customizing bar chart

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-d3.select('div')
-    .selectAll('p')
-    .data([1, 2, 3])
+const DUMMY_DATA = [
+    {id: 'd1', value: 10, region: 'USA'},
+    {id: 'd2', value: 11, region: 'Germany'},
+    {id: 'd3', value: 12, region: 'China'},
+    {id: 'd4', value: 13, region: 'India'}
+]
+
+const container = d3.select('div')
+    .classed('container', true)
+    .style('border', '1px solid red');
+
+container.selectAll('.bar')
+    .data(DUMMY_DATA)
     .enter()
-    .append('p')
-    .text(dta => dta);
+    .append('div')
+    .classed('bar', true)
+    .style('width', '50px')
+    .style('height', data=> (data.value * 15) + 'px');
